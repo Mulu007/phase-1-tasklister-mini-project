d71419c3fe10d2563a9048bc768decb3a5419920
Mulu <brianmulu007@gmail.com>
2022-09-07 14:48:50 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  let list = document.querySelectorAll('#list');
+  // console.log(list);
+
+  let input = document.querySelector('#new-task-description')
+  let formTask = document.querySelector('#create-task-form');
+
+  console.log(formTask);
+  formTask.addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      let list = document.querySelector('#tasks')
+      let listContent = document.createElement('li')
+      listContent.innerText = input.value
+      let deleteButton = document.createElement('button')
+      deleteButton.innerText = 'x'
+
+      list.appendChild(listContent);
+      listContent.appendChild(deleteButton);
+      formTask.reset()
+      deleteButton.addEventListener('click', function(e) {
+
+          listContent.remove();
+      })
+      //resets the form input
+      formTask.reset()
+  })
+
+  })
+
+
